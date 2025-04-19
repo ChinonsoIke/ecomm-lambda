6972cf6b0d0c9a6ab5cae204b68f0d6da2e9ab82
Chinonso Ikewelugo <chinonso.ikewelugo@pwcng-rpa.com>
2025-04-19 11:24:34 -0500

    first commit

---
+++ ecomm-lambda.mjs
@@ -0,0 +1,150 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { ScanCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+
+const REGION = "us-east-2";
+const dynamoDB = new DynamoDBClient({ region: REGION });
+let claims = null;
+
+export const handler = async (event) => {
+  try {
+      claims = event.requestContext?.authorizer?.claims;
+      if (!claims) {
+        return {
+          statusCode: 401,
+          body: JSON.stringify({ message: "Unauthorized" }),
+        };
+      }
+      
+      console.log(event);
+      const evt = JSON.parse(event.body);
+      if (evt.type == "search"){
+        return await search(evt.searchTerm);
+      } else if (evt.type == "addToCart"){
+        return await addToCart(evt.productId);
+      } else if (evt.type == "order"){
+        return await order(evt.productIds);
+      } else return await getProducts();
+  } catch (error) {
+    console.error("Something went wrong:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Something went wrong" }),
+    };
+  }
+}
+
+async function getProducts(){
+  try {
+    const command = new ScanCommand({
+      TableName: "Products",
+    });
+
+    const result = await dynamoDB.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+  } catch (error) {
+    console.error("Error scanning table:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to scan table" }),
+    };
+  }
+}
+
+async function search(searchTerm) {
+  console.log("Search term:", searchTerm);
+  const command = new ScanCommand({
+      TableName: "Products",
+      FilterExpression: "contains(title, :val)",
+      ExpressionAttributeValues: {
+        ":val": searchTerm,
+      },
+  });
+  
+  try {
+      const result = await dynamoDB.send(command);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(result.Items),
+      };
+  } catch (err) {
+      console.error("Search failed:", err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "Search failed" }),
+      };
+  }
+}
+
+async function addToCart(productId) {
+    //get user cart
+    const getCart = new ScanCommand({
+        TableName: "Carts",
+        FilterExpression: "userId = :userId",
+        ExpressionAttributeValues: {
+          ":userId": claims.sub,
+        },
+    });
+
+    const result = await dynamoDB.send(getCart);
+    const cart = result.Items?.[0];
+    if(cart == null) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "Add to cart failed: user does not have a cart" })
+          };
+    }
+      
+    const command = new PutCommand({
+        TableName: "CartItems",
+        Item: {
+          id: Date.now().toString(36),
+          cartId: cart.id,
+          productId: productId
+        }
+    });
+    
+    try {
+        const result = await dynamoDB.send(command);
+        console.log("Item added to cart:", result);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: "Item added to cart" })
+        };
+    } catch (err) {
+        console.error("Add to cart failed:", err);
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ error: "Add to cart failed" })
+        };
+    }
+}
+
+async function order(productIds) {
+    const command = new PutCommand({
+        TableName: "Orders",
+        Item: {
+          id: Date.now().toString(36),
+          userId: claims.sub,
+          productIds: productIds
+        }
+    });
+    
+    try {
+        const result = await dynamoDB.send(command);
+        console.log("Order placed:", result);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: "Order placed" })
+        };
+    } catch (err) {
+        console.error("Order failed:", err);
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ error: "Order failed" })
+        };
+    }
+}
